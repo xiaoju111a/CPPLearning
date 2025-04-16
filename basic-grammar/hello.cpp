113e767a5bcadc7e3d818d035294e424be1bf5b3
Tim <62780864+xiaoju111a@users.noreply.github.com>
2025-04-16 10:53:36 +0000

    day1

---
+++ basic-grammar/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "hello world!" << endl; //输出hello world
+	bool flag = 1;
+	char c = 'a';
+	int k = 5;
+	float f = 3.14;
+	double d = 4.1;
+	wchar_t w = '你';
+	cout << flag << " " << c << " " << k << " " << f << " " << d << " " << w << endl;
+	return 0;
+}
