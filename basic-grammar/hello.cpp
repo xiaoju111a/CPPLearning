8d6170617c315672f8c035412cdc8747074993e4
Tim <62780864+xiaoju111a@users.noreply.github.com>
2025-04-16 11:59:40 +0000

    finish

--- basic-grammar/hello.cpp
+++ basic-grammar/hello.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main() {
 	cout << "hello world!" << endl; //输出hello world
@@ -12,5 +13,13 @@ int main() {
 	string str = "hello, \
 		      world";
 	cout << str << endl;
+	vector<int> q;
+	for(int i = 1; i <= 10; i++) {
+		q.push_back(i);
+		cout << q[i-1] << endl;
+	}
+	for(auto i = q.begin(); i != q.end(); i++) {
+		cout << *i << " ";
+	}
 	return 0;
 }
